5ec4c1a3890f7a617de04841146e37a07002755b
OndraZizka <zizka@seznam.cz>
2013-07-08 06:47:36 +0200

    Make SoutCopyingFileWriter create parent dirs.

--- testbase/src/main/java/org/jboss/jawabot/testbase/SoutCopyingFileWriter.java
+++ testbase/src/main/java/org/jboss/jawabot/testbase/SoutCopyingFileWriter.java
@@ -2,16 +2,33 @@
 package org.jboss.jawabot.testbase;
 
 import java.io.*;
+import org.apache.commons.io.FileUtils;
 
 
 /**
- *
- * @author Ondrej Zizka
+ *  A FileWriter which copies everything written to it to standard output.
+ * 
+ *  @author Ondrej Zizka
  */
 public class SoutCopyingFileWriter extends FileWriter {
 
-   public SoutCopyingFileWriter(File file) throws IOException {      super(file);   }
-   public SoutCopyingFileWriter(String fileName) throws IOException {      super(fileName);   }
+   public SoutCopyingFileWriter(File file) throws IOException {
+       super( createParentDirs(file) );
+   }
+   public SoutCopyingFileWriter(String fileName) throws IOException {
+       super( createParentDirs(fileName) );
+   }
+   
+   private static File createParentDirs( File file ) throws IOException {
+       FileUtils.forceMkdir( file.getParentFile() );
+       return file;
+   }
+
+   private static String createParentDirs( String fileName ) throws IOException {
+       File file = new File( fileName );
+       FileUtils.forceMkdir( file.getParentFile() );
+       return fileName;
+   }
 
    @Override
    public void write(int c) throws IOException {
